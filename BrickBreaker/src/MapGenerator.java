03df9d1796b513ca5c46acdafe82f16aa25c1303
JessicaAngela <114288401+JessicaAngelah@users.noreply.github.com>
2023-06-16 20:32:08 +0700

    Update MapGenerator.java

--- BrickBreaker/src/MapGenerator.java
+++ BrickBreaker/src/MapGenerator.java
@@ -8,6 +8,7 @@ public class MapGenerator
 	public int brickWidth;
 	public int brickHeight;
 	
+	//create and set up the initial state of the game
 	public MapGenerator (int row, int col)
 	{
 		map = new int[row][col];		
@@ -31,8 +32,8 @@ public class MapGenerator
 			{
 				if(map[i][j] > 0)
 				{
-					g.setColor(Color.lightGray);
-					g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+					g.setColor(Color.lightGray); // color of the brick's border
+					g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight); //shows the brick's width and height
 					
 					// this is just to show separate brick, game can still run without it
 					g.setStroke(new BasicStroke(3));
@@ -43,7 +44,7 @@ public class MapGenerator
 			}
 		}
 	}
-	
+	//sets a specific value in the map at the specified row and column.
 	public void setBrickValue(int value, int row, int col)
 	{
 		map[row][col] = value;
