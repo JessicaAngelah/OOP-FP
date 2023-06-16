4ed18dc5dc08a0c3a7accbc6388bfe3963b85bc3
JessicaAngela <114288401+JessicaAngelah@users.noreply.github.com>
2023-06-16 20:28:08 +0700

    Update Datastore.java

--- BrickBreaker/src/Datastore.java
+++ BrickBreaker/src/Datastore.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Datastore {
     public static int highScore = 0;
-
+// saves high score in the data.txt file
     public static void saveHighScore(int score) {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("data.txt"))) {
             outputStream.writeObject(score);
@@ -10,11 +10,11 @@ public class Datastore {
             e.printStackTrace();
         }
     }
-
+// resets highscore
     public static void resetHighScore() {
         saveHighScore(0);
     }
-
+// responsible for loading the high score from the data.txt file
     public static void loadHighScore() {
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("data.txt"))) {
             highScore = (int) inputStream.readObject();
@@ -22,7 +22,7 @@ public class Datastore {
             highScore = 0;
         }
     }
-
+//retrieves and returns the current value of the highScore (allows other parts of the program to access and use the high score value stored in the variable)
     public static int getHighScore() {
         return highScore;
     }
